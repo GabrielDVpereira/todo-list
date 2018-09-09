75874cd82c7579120bf64c1ea435635f96dc6e0d
GabrielDVpereira <gdavi.gd@gmail.com>
2018-09-09 17:34:40 -0300

    Added the link to delete, create and edit new items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,13 +16,34 @@ before_action :find_item, only: [:show, :edit, :update, :destroy]
       render "new"
     end
   end
+
   def show
   end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render 'edit'
+    end
+  end
+
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
   private
   def item_params
     params.require(:item).permit(:title, :description)
   end
+
   def find_item
     @item=Item.find(params[:id])
   end
+
 end
